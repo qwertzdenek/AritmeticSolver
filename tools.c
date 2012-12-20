79dcd01416840c140af50ea98043d1af2d33b1cd
Zdeněk Janeček <jan.zdenek@gmail.com>
2012-12-20 21:36:40 +0100

    Help message that shows license and author.

--- tools.c
+++ tools.c
@@ -3,13 +3,20 @@
 #include <string.h>
 #include <stdbool.h>
 
+void help()
+{
+  printf("LISP style solver by Zdeněk Janeček 2012\n");
+  printf("This program comes with ABSOLUTELY NO WARRANTY!\n");
+  printf("This is free software, and you are welcome to redistribute it under conditions of the GNU GPL license version 3.\n");
+}
+
 void die(const char *msg)
 {
   fprintf(stderr, "%s\n", msg);
   exit(EXIT_FAILURE);
 }
 
-bool equals(char *compared, char *to)
+bool equals(const char *compared, const char *to)
 {
   int len_compared = strlen(compared);
   int len_to = strlen(to);
--- tools.h
+++ tools.h
@@ -1,7 +1,8 @@
 #ifndef _TOOLS_H
 #define _TOOLS_H
 
+void help();
 void die(const char *msg);
-bool equals(char *compared, char *to);
+bool equals(const char *compared, const char *to);
 
 #endif
